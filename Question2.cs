51c1826fce5323f03608b30f78f5e9295e119ade
Mert Dedeoğlu <87008233+mertdedeoglu@users.noreply.github.com>
2022-06-06 23:53:34 +0300

    Update Question2.cs

--- Question2.cs
+++ Question2.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
-
+/*Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). 
+Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
+Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.*/
 namespace ConsoleApp1
 {
     class Program
